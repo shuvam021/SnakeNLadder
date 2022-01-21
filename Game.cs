a0282bd60d7ea0201ab703719d05846e6538eaae
Shuvam Das <repository.shuvam@gmail.com>
2022-01-21 17:15:50 +0530

    [Shuvam] Add current position updated according to option

--- Game.cs
+++ Game.cs
@@ -8,13 +8,35 @@ namespace SnakeNLadder
     internal class Game
     {
         private static int _currentPosition = 0;
+        private const int _optionNoPlay = 0;
+        private const int _optionLadder = 1;
+        private const int _optionSanke = 2;
         private static Random random = new Random();
+
+        /// <summary>Updates the current position based on option.</summary>
+        /// <param name="option">snake(2)/ladder(1)/no_paly(0)</param>
+        /// <param name="position">Current position</param>
+        /// <param name="roll">expected values are 1-6</param>
+        private static int UpdatePosition(int option, int position, int roll)
+        {
+            switch (option)
+            {
+                case _optionLadder:
+                    if (position + roll < 100) position += roll;
+                    break;
+                case _optionSanke:
+                    if (position - roll > 0) position -= roll;
+                    break;
+            }
+            return position;
+        }
         /// <summary>Use this Game class with only this method.</summary>
         public static void Play()
         {
             int rollDice = random.Next(1, 7);
-            Console.WriteLine($"You are at: {_currentPosition}");
-            Console.WriteLine($"You got: {rollDice}");
+            int option = random.Next(0, 3);
+            _currentPosition = UpdatePosition(option, _currentPosition, rollDice);
+            Console.WriteLine($"You got: {option}/{rollDice} and Position is at: {_currentPosition}");
         }
     }
 }
