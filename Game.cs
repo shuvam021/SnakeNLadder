af239c216bcb40759a1145a2bc61311b68dc4f06
Shuvam Das <repository.shuvam@gmail.com>
2022-01-21 15:55:47 +0530

    [Shuvam] initial commit

---
+++ Game.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SnakeNLadder
+{
+    internal class Game
+    {
+        public static void Solution()
+        {
+            Console.WriteLine("Welcome to Snake Ladder Game");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SnakeNLadder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Game.Solution();
+        }
+    }
+}
