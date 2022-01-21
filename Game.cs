331a862d8489fdb1872ae0372edafbcc1c414a1b
Shuvam Das <repository.shuvam@gmail.com>
2022-01-21 16:41:41 +0530

    [Shuvam] Add roll die feature added

--- Game.cs
+++ Game.cs
@@ -4,12 +4,17 @@ using System.Text;
 
 namespace SnakeNLadder
 {
+    /// <summary>Snake and Ladder game.</summary>
     internal class Game
     {
         private static int _currentPosition = 0;
-        public static void Solution()
+        private static Random random = new Random();
+        /// <summary>Use this Game class with only this method.</summary>
+        public static void Play()
         {
-            Console.WriteLine($"current position is at {_currentPosition}");
+            int rollDice = random.Next(1, 7);
+            Console.WriteLine($"You are at: {_currentPosition}");
+            Console.WriteLine($"You got: {rollDice}");
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace SnakeNLadder
     {
         static void Main(string[] args)
         {
-            Game.Solution();
+            Game.Play();
         }
     }
 }
